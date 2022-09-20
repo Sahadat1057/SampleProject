05387de96a57f40d3297f412786368896696323f
sahadatamzad <samzad7698@bths.edu>
2022-09-20 19:26:59 -0400

    I have added favorite food

--- src/Sample.java
+++ src/Sample.java
@@ -1,7 +1,8 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Sahadat Hossen Amzad");
-        System.out.print("My favorite movie of all time is Shawshank Redemption.");
+        System.out.println("My favorite movie of all time is Shawshank Redemption.");
+        System.out.print("My favorite food is pizza.");
 
     }
 }
