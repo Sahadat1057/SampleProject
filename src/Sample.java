d923e40e06c90ed31fb27b22fc12aada48dda1c4
BT_4N2_11 <samzad7698@bths.edu>
2022-09-20 11:42:59 -0400

    I added print statement for movie

--- src/Sample.java
+++ src/Sample.java
@@ -1,6 +1,7 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Sahadat Hossen Amzad");
+        System.out.print("My favorite movie of all time is Shawshank Redemption.");
 
     }
 }
